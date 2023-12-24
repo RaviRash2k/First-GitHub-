46167c71493434a152dff1fdf253d39ea56dcba6
Ravi Rashmitha <150897047+RaviRash2k@users.noreply.github.com>
2023-12-24 17:46:13 +0530

    Add files via upload

---
+++ helloGithub.java
@@ -0,0 +1,15 @@
+package Pkg1;
+
+public class helloGithub {
+
+    String x;
+
+    public static void main(String[] args) {
+
+        String x = "Github";
+
+        System.out.println("Hello " + x + "!");
+
+    }
+
+}
